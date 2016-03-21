ae4f83cd5444e8cab6a9b76eb58ebdf54e611371
RaoulMeyer <raouljmeyer@gmail.com>
2016-03-21 22:49:45 +0100

    Added partitioning and shuffling

--- src/manager.erl
+++ src/manager.erl
@@ -34,12 +34,12 @@ save_array_to_files([First | Rest], Counter) ->
 
 %%% map %%%
 map(M, R, InputFilesCount) ->
-    Workers = spawn_workers(3),
+    Workers = spawn_workers(4),
     timer:sleep(1000),
     map(M, R, 0, InputFilesCount, Workers).
 
 map(M, R, ProcessedFiles, TotalFiles, Workers) when ProcessedFiles == TotalFiles + 1 ->
-    timer:sleep(2000),
+    timer:sleep(1000),
 %    cleanup_workers(Workers),
     sort(R, TotalFiles, Workers);
 map(M, R, ProcessedFiles, TotalFiles, Workers) ->
@@ -59,6 +59,50 @@ spawn_workers(Count) ->
     AllWorkerPids = [WorkerPid] ++ spawn_workers(Count - 1),
     AllWorkerPids.
 
+%%% sort %%%
+sort(R, TotalFiles, Workers) ->
+    message_workers({self(), sort}, Workers),
+    sort(R, TotalFiles, Workers, 0, [], []).
+
+sort(R, TotalFiles, Workers, TotalSorted, MinValues, MaxValues) when length(Workers) == TotalSorted ->
+    partition(R, Workers, MinValues, MaxValues);
+sort(R, TotalFiles, Workers, TotalSorted, MinValues, MaxValues) ->
+    receive
+        {From, sorted, Min, Max} ->
+            NewMinValues = MinValues ++ [Min],
+            NewMaxValues = MaxValues ++ [Max]
+    end,
+    sort(R, TotalFiles, Workers, TotalSorted + 1, NewMinValues, NewMaxValues).
+
+message_workers(_, []) -> ok;
+message_workers(Message, [FirstWorker | Rest]) ->
+    FirstWorker ! Message,
+    message_workers(Message, Rest).
+
+
+%%% partition %%%
+partition(R, Workers, MinValues, MaxValues) ->
+    Partitioning = build_partitioning(Workers, lists:min(MinValues), lists:max(MaxValues)),
+    message_workers({self(), partition, Partitioning}, Workers),
+    timer:sleep(5000),
+    cleanup_workers(Workers),
+    flush().
+
+build_partitioning(Workers, Min, Max) ->
+    build_partition_for_workers(Workers, length(Workers), Min, Max, 0).
+
+build_partition_for_workers([], _, _, _, _) -> [];
+build_partition_for_workers([FirstWorker | Rest], WorkerCount, Min, Max, Position) ->
+    SmallestLetter = lists:nth(1, binary_to_list(Min)) * 256 - 1,
+    LargestLetter = lists:nth(1, binary_to_list(Max)) * 256 + 1,
+    StartingLetters = SmallestLetter + (LargestLetter - SmallestLetter) / WorkerCount * Position,
+    EndingLetters = SmallestLetter + (LargestLetter - SmallestLetter) / WorkerCount * (Position + 1),
+    [{FirstWorker, integer_to_two_letters(StartingLetters), integer_to_two_letters(EndingLetters)}]
+        ++ build_partition_for_workers(Rest, WorkerCount, Min, Max, Position + 1).
+
+integer_to_two_letters(Integer) ->
+    [trunc(Integer / 256), trunc(Integer) rem 256].
+
 cleanup_workers([]) -> ok;
 cleanup_workers([FirstWorker | Rest]) ->
     exit(FirstWorker, kill),
@@ -69,22 +113,3 @@ flush() ->
     after
         0 -> ok
     end.
-
-%%% sort %%%
-sort(R, TotalFiles, Workers) ->
-    message_workers({self(), sort}, Workers),
-    timer:sleep(2000),
-    flush(),
-%    cleanup_workers(Workers),
-    timer:sleep(1000),
-    Workers.
-
-% sort(R, TotalFiles, Workers, )
-
-message_workers(_, []) -> ok;
-message_workers(Message, [FirstWorker | Rest]) ->
-    FirstWorker ! Message,
-    message_workers(Message, Rest).
-
-
-%%% partition %%%
--- src/worker.erl
+++ src/worker.erl
@@ -16,11 +16,27 @@ loop(Manager, Id) ->
         {From, map, Map, File} ->
             map(Map, File, Id),
             timer:sleep(1000),
-            Manager ! {self(), idle};
+            Manager ! {self(), idle},
+            loop(Manager, Id);
         {From, sort} ->
-            sort(Id)
+            Sorted = sort(Id),
+            {Min, _} = lists:nth(1, Sorted),
+            {Max, _} = lists:last(Sorted),
+            Manager ! {self(), sorted, Min, Max},
+            loop(Manager, Id);
+        {From, partition, Partitioning} ->
+            send_partitioned_data(Partitioning, get_mapped_items(Id)),
+            loop(Manager, Id, [])
+    end.
+
+loop(Manager, Id, Data) ->
+    receive
+        {From, partitiondata, ReceivedData} ->
+            NewData = Data ++ ReceivedData,
+            io:format("Data received in ~p, full data: ~p~n", [self(), NewData])
     end,
-    loop(Manager, Id).
+    loop(Manager, Id, NewData).
+
 
 map(Map, File, Id) ->
     {ok, Data} = file:read_file("../generated/split" ++ integer_to_list(File) ++ ".txt"),
@@ -39,7 +55,8 @@ sort(Id) ->
     timer:sleep(1000),
     Items = get_mapped_items(Id),
     SortedItems = sort_mapped_items(Items),
-    io:format("Sorted items: ~p~n", [SortedItems]).
+    io:format("Sorted items: ~p~n", [SortedItems]),
+    SortedItems.
 
 get_mapped_items(Id) ->
     {ok, Data} = file:read_file("../generated/mapped" ++ integer_to_list(Id) ++ ".txt"),
@@ -58,3 +75,10 @@ sort_mapped_items([Pivot | Rest]) ->
     sort_mapped_items([{ItemKey, ItemValue} || {ItemKey, ItemValue} <- Rest, ItemKey =< PivotKey])
 	++ [Pivot] ++
     sort_mapped_items([{ItemKey, ItemValue} || {ItemKey, ItemValue} <- Rest, ItemKey > PivotKey]).
+
+send_partitioned_data([], _) -> [];
+send_partitioned_data([Partition | Rest], Items) ->
+    {Target, From, To} = Partition,
+    Data = [{Key, Value} || {Key, Value} <- Items, From < binary_to_list(Key), binary_to_list(Key) < To],
+    Target ! {self(), partitiondata, Data},
+    send_partitioned_data(Rest, Items).
